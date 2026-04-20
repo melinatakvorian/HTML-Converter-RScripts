e3ebfb830d86c8e6f20c3e93bfe05c3bd5a1668f
Melina Takvorian <takvorian.melina@gmail.com>
2026-04-20 09:17:32 -0600

    RAF Veg conversion

--- DocToExcelConverter.R
+++ DocToExcelConverter.R
@@ -32,10 +32,10 @@ invisible(lapply(packages, library, character.only = TRUE))
     input_umbrella <- 
       "N:/RStor/CEMML/ClimateChange/1_USAFClimate/1_USAF_Natural_Resources/20_2_0004_RevisitingPhase1/" 
     #the specific folder inside the Document to HTML Table Converter where the input files are
-    input_specific_folder <- "Eielson AFB/TEVA/Word to HTML Conversion"
+    input_specific_folder <- "RAF Alconbury_Molesworth/TerrestrialVegetation/Word to HTML Conversion"
   
   #the final file name will start with this and will get the date added
-    project_name <- "EielsonAFB_run1_TEVA" #Replace with whatever you want.
+    project_name <- "RAFAlconburyMolesworth_Veg_01" #Replace with whatever you want.
 
 #####NO MORE CHANGES --- -- -- -- --- - - -- -- - -  - - - - -  --- - - - - - - --- --- --- -- ---
 

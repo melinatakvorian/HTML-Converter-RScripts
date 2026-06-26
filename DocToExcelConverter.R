de8e9439745ca9bd3dc8f66b15bf2c3f8985a51a
Kaylin Thomas <kaylin.thomas9@gmail.com>
2026-06-26 15:05:24 -0600

    T&E - Norfolk - Navy. Updated the Installation_Info.xlsx location

--- DocToExcelConverter.R
+++ DocToExcelConverter.R
@@ -30,24 +30,24 @@ invisible(lapply(packages, library, character.only = TRUE))
   #PAY ATTENTION TO THE DIRECTION OF THE SLASHES. THEY HAVE TO BE CHANGED TO FORWARD SLASHES, AS SHOWN BELOW
     #the broad folder structure
     #AIR FORCE  
-    input_umbrella <- "N:/RStor/CEMML/ClimateChange/1_USAFClimate/1_USAF_Natural_Resources/20_2_0004_RevisitingPhase1/"
+    #input_umbrella <- "N:/RStor/CEMML/ClimateChange/1_USAFClimate/1_USAF_Natural_Resources/20_2_0004_RevisitingPhase1/"
     
     #NAVY
-    # input_umbrella <- "N:/RStor/CEMML/ClimateChange/2_NavyClimate/Round2_Extremes_INRMP_integ/Southeast Region/"
+    input_umbrella <- "N:/RStor/CEMML/ClimateChange/2_NavyClimate/Round2_Extremes_INRMP_integ/MidLant Region/"
 
     #the specific folder inside the Document to HTML Table Converter where the input files are
-    input_installation_folder <- "Kokee AFS" #corresponds to shortName on the installation_info.xlsx
-    input_SME_folder <- "/WildlandFire/Word to HTML Conversion"
+    input_installation_folder <- "NS Norfolk" #corresponds to shortName on the installation_info.xlsx
+    input_SME_folder <- "/T&E/Word to HTML Conversion"
   
   #the final file name will start with this and will get the date added
-    subject <- "Fire"
+    subject <- "T&E"
     project_name <- paste0(subject, "_", input_installation_folder) 
 
 #####NO MORE CHANGES --- -- -- -- --- - - -- -- - -  - - - - -  --- - - - - - - --- --- --- -- ---
 
   input_dir <-  paste0(input_umbrella, input_installation_folder, input_SME_folder)
   current_date <- format(Sys.Date(), "%Y%m%d")  # e.g., "2025-09-24"
-  installation_info <- readxl::read_xlsx("N:/RStor/CEMML/ClimateChange/1_USAFClimate/1_USAF_Natural_Resources/20_2_0004_RevisitingPhase1/_AirForceClimateViewerDev/Document to HTML Table Converter/FilesForTesting/Installation_Info.xlsx")
+  installation_info <- readxl::read_xlsx("N:/RStor/CEMML/ClimateChange/Document Standards/Templates/TEMPLATES_SME_Word_Docs/Installation_Info.xlsx")
 
 #ERROR CATCH: open files ----
   

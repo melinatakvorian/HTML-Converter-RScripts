d72bb23fcecc16b079344db6838927488e94b127
Molly Arndt <mollyjarndt@gmail.com>
2026-08-07 10:59:09 -0600

    Indian Mountain Hydro Conversion

--- Hydro-AF-NAVY-Converter.R
+++ Hydro-AF-NAVY-Converter.R
@@ -31,7 +31,7 @@ invisible(lapply(packages, library, character.only = TRUE))
  
 # ----TEXT FOR YOU TO CHANGE-----------
   # Select which installation folder you're working in
-  input_installation_folder <- "Cold Bay LRRS"
+  input_installation_folder <- "Indian Mountain LRRS"
 
   # Write if working on AF (AIR FORCE) or Navy (NAVY):
   # inst_sheet = "NAVY"
